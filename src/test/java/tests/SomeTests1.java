0e8adc5cd9d69cd3f4cfc509c69c576f1c7e70d5
sltnwlf <farkas.zoli.90@gmail.com>
2022-07-18 17:22:55 +0200

    allure test

---
+++ src/test/java/tests/SomeTests1.java
@@ -0,0 +1,29 @@
+package tests;
+
+import base.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SomeTests1 extends TestBase {
+
+    @Test
+    @DisplayName("")
+    public void testSome1() {
+        somePage1.popupKiller();
+        String actual = somePage1.getSiteNameText();
+        String expected = "Selenium Easy";
+        Assertions.assertEquals(actual,expected);
+    }
+    @Test
+    @DisplayName("")
+    public void testSome2() {
+        somePage1.popupKiller();
+    }
+    @Test
+    @DisplayName("")
+    public void testSome3() {
+        somePage1.popupKiller();
+    }
+
+}
